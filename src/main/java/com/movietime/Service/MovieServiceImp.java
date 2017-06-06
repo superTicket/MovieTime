594040f46179f1a5afe9bfd52e1c000370dff47c
yangziy <yangziy@outlook.com>
2017-06-06 21:15:38 +0800

    BUG FIX

--- src/main/java/com/movietime/Service/MovieServiceImp.java
+++ src/main/java/com/movietime/Service/MovieServiceImp.java
@@ -1,6 +1,6 @@
 package com.movietime.Service;
 
-import com.movietime.VO.MovieforDisplay;
+import com.movietime.entity.Movie;
 import org.springframework.stereotype.Component;
 
 import java.util.LinkedList;
@@ -11,15 +11,15 @@ import java.util.List;
  */
 @Component
 public class MovieServiceImp implements MovieService {
-    public List<MovieforDisplay> findAll() {
-        List<MovieforDisplay> movieList = new LinkedList<MovieforDisplay>();
-        movieList.add(new MovieforDisplay("Guardians of the Galaxy", null, "/images/homepage/banner.jpg"));
-        movieList.add(new MovieforDisplay("Transformers 4", null, "/images/homepage/banner1.jpg"));
-        movieList.add(new MovieforDisplay("Game of Thrones", null, "/images/homepage/banner2.jpg"));
+    public List<Movie> findAll() {
+        List<Movie> movieList = new LinkedList<Movie>();
+        movieList.add(new Movie("Guardians of the Galaxy", null, "/images/homepage/banner.jpg"));
+        movieList.add(new Movie("Transformers 4", null, "/images/homepage/banner1.jpg"));
+        movieList.add(new Movie("Game of Thrones", null, "/images/homepage/banner2.jpg"));
         return movieList;
     }
 
-    public MovieforDisplay findOne(int id) {
+    public Movie findOne(int id) {
         return null;
     }
 
@@ -32,32 +32,32 @@ public class MovieServiceImp implements MovieService {
         return tagList;
     }
 
-    public List<MovieforDisplay> getMovieByTag(String tag) {
-        List<MovieforDisplay> movieList = new LinkedList<MovieforDisplay>();
+    public List<Movie> getMovieByTag(String tag) {
+        List<Movie> movieList = new LinkedList<Movie>();
         if (tag.equals("Featured")) {
-            movieList.add(new MovieforDisplay("God’s Compass", "/images/homepage/featureMoviesImages/m15.jpg"));
-            movieList.add(new MovieforDisplay("Bad Moms", "/images/homepage/featureMoviesImages/m2.jpg"));
-            movieList.add(new MovieforDisplay("Jason Bourne", "/images/homepage/featureMoviesImages/m5.jpg"));
-            movieList.add(new MovieforDisplay("Rezort", "/images/homepage/featureMoviesImages/m16.jpg"));
-            movieList.add(new MovieforDisplay("Peter", "/images/homepage/featureMoviesImages/m17.jpg"));
-            movieList.add(new MovieforDisplay("ISRA 88", "/images/homepage/featureMoviesImages/m18.jpg"));
-            movieList.add(new MovieforDisplay("WAR DOGS", "/images/homepage/featureMoviesImages/m1.jpg"));
-            movieList.add(new MovieforDisplay("The Other Side", "/images/homepage/featureMoviesImages/m14.jpg"));
-            movieList.add(new MovieforDisplay("Civil War", "/images/homepage/featureMoviesImages/m19.jpg"));
-            movieList.add(new MovieforDisplay("The Secret Life of Pets", "/images/homepage/featureMoviesImages/m20.jpg"));
-            movieList.add(new MovieforDisplay("The Jungle Book", "/images/homepage/featureMoviesImages/m21.jpg"));
-            movieList.add(new MovieforDisplay("Assassin's Creed 3", "/images/homepage/featureMoviesImages/m22.jpg"));
+            movieList.add(new Movie("God’s Compass", "/images/homepage/featureMoviesImages/m15.jpg"));
+            movieList.add(new Movie("Bad Moms", "/images/homepage/featureMoviesImages/m2.jpg"));
+            movieList.add(new Movie("Jason Bourne", "/images/homepage/featureMoviesImages/m5.jpg"));
+            movieList.add(new Movie("Rezort", "/images/homepage/featureMoviesImages/m16.jpg"));
+            movieList.add(new Movie("Peter", "/images/homepage/featureMoviesImages/m17.jpg"));
+            movieList.add(new Movie("ISRA 88", "/images/homepage/featureMoviesImages/m18.jpg"));
+            movieList.add(new Movie("WAR DOGS", "/images/homepage/featureMoviesImages/m1.jpg"));
+            movieList.add(new Movie("The Other Side", "/images/homepage/featureMoviesImages/m14.jpg"));
+            movieList.add(new Movie("Civil War", "/images/homepage/featureMoviesImages/m19.jpg"));
+            movieList.add(new Movie("The Secret Life of Pets", "/images/homepage/featureMoviesImages/m20.jpg"));
+            movieList.add(new Movie("The Jungle Book", "/images/homepage/featureMoviesImages/m21.jpg"));
+            movieList.add(new Movie("Assassin's Creed 3", "/images/homepage/featureMoviesImages/m22.jpg"));
         } else if (tag.equals("Top viewed")) {
-            movieList.add(new MovieforDisplay("MovieforDisplay 3", "/images/homepage/featureMoviesImages/m3.jpg"));
-            movieList.add(new MovieforDisplay("MovieforDisplay 4", "/images/homepage/featureMoviesImages/m4.jpg"));
+            movieList.add(new Movie("MovieforDisplay 3", "/images/homepage/featureMoviesImages/m3.jpg"));
+            movieList.add(new Movie("MovieforDisplay 4", "/images/homepage/featureMoviesImages/m4.jpg"));
 
         } else if (tag.equals("Top Rating")) {
-            movieList.add(new MovieforDisplay("MovieforDisplay 5", "/images/homepage/featureMoviesImages/m5.jpg"));
-            movieList.add(new MovieforDisplay("MovieforDisplay 6", "/images/homepage/featureMoviesImages/m6.jpg"));
+            movieList.add(new Movie("MovieforDisplay 5", "/images/homepage/featureMoviesImages/m5.jpg"));
+            movieList.add(new Movie("MovieforDisplay 6", "/images/homepage/featureMoviesImages/m6.jpg"));
 
         } else if (tag.equals("Recently Added")) {
-            movieList.add(new MovieforDisplay("MovieforDisplay 7", "/images/homepage/featureMoviesImages/m7.jpg"));
-            movieList.add(new MovieforDisplay("MovieforDisplay 8", "/images/homepage/featureMoviesImages/m8.jpg"));
+            movieList.add(new Movie("MovieforDisplay 7", "/images/homepage/featureMoviesImages/m7.jpg"));
+            movieList.add(new Movie("MovieforDisplay 8", "/images/homepage/featureMoviesImages/m8.jpg"));
         } else {
             movieList = null;
         }

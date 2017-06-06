ef978fd260c3bda4f187840659a06246d9c44a13
yangziy <yangziy@outlook.com>
2017-06-06 20:47:47 +0800

    第二轮重构
    1. HomeController现在以面向DAO接口的方式获取数据
    2. 增加了会话管理，相关代码目前在LoginController和HomeController中

--- src/main/java/com/movietime/Service/CinemaInfoService.java
+++
@@ -1,8 +0,0 @@
-package com.movietime.Service;
-
-/**
- * Created by yangzy on 2017/6/5.
- */
-public interface CinemaInfoService {
-    int getCinemaNameById();
-}
--- src/main/java/com/movietime/Service/ImageService.java
+++
@@ -1,7 +0,0 @@
-package com.movietime.Service;
-
-/**
- * Created by yangzy on 2017/6/6.
- */
-public class ImageService {
-}
